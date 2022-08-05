415e2a392891355b39890f63a4a291040094cb51
Barendan <barendan@gmail.com>
2022-08-05 08:28:46 -0400

    add header styling for cards

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,15 +5,25 @@ const BlogList = ({ blogs }) => (
   <div className="blog-list">
     <Card.Group>
       { blogs?.getPosts.map(blog => (
-        <Card 
+        // <Card 
+        //   fluid
+        //   header={blog.title}
+        //   meta={`written by ${blog.author}`}
+        //   description={blog.body}
+        //   // color='red' 
+        // />
+        
+        <Card
           fluid
           key={blog._id}
           href={`/blogs/${blog._id}`}
-          header={blog.title}
-          meta={`written by ${blog.author}`}
-          description={blog.body}
-          // color='red' 
-        />
+        >
+          <Card.Content>
+            <Card.Header className="card-header">{blog.title}</Card.Header>
+            <Card.Meta>{`written by ${blog.author}`}</Card.Meta>
+            <Card.Description>{blog.body}</Card.Description>
+          </Card.Content>
+        </Card>
       ))}
     </Card.Group>
   </div>
