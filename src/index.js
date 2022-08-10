15147b059c59b1a5437f82fb737e2b131318cca1
Barendan <barendan@gmail.com>
2022-08-10 11:06:31 -0400

    add http and auth link from apollo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { 
+  ApolloClient, 
+  ApolloProvider, 
+  InMemoryCache,
+  createHttpLink 
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.NODE_ENV !== 'production'
+  ? 'http://localhost:4000'
+  : process.env.REACT_APP_GQL_SERVER
+})
+
+const authLink = setContext((_, {headers}) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
 
 const client = new ApolloClient({
   // uri: 'https://namblo-server.herokuapp.com/',
